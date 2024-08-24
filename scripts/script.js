12799576b451461600abd18b1c0fb21f2fc4941c
Floran05 <carvalhofloran@gmail.com>
2024-08-24 16:37:27 +0200

    Initial commit

---
+++ scripts/script.js
@@ -0,0 +1,53 @@
+(function(){
+
+    window.onload = function(){
+
+        document.querySelectorAll('a[href^="#"]').forEach(el => {
+            el.addEventListener('click', e => {
+                e.preventDefault();
+
+                document.querySelector(e.currentTarget.getAttribute('href')).scrollIntoView({
+                    behavior: 'smooth'
+                });
+            });
+        });
+        document.querySelectorAll('.close-popup, #popups').forEach(el => {
+            el.addEventListener('click', e => {
+                if(el.id === 'popups' && e.target != el) return;
+                const popups = document.getElementById('popups');
+                if(popups) {
+                    popups.style.display = 'none';
+                    popups.classList.remove('show');
+                }
+            });
+        });
+        document.getElementById('button-show-paragon-popup').addEventListener('click', e => showPopup('paragon'));
+
+        let copyTimeout = null;
+        document.getElementById('mail-text').addEventListener('click', e => {
+            if(copyTimeout) return;
+
+            const value = e.currentTarget.textContent;
+            navigator.clipboard.writeText(value);
+            e.currentTarget.textContent = "Copiée !";
+            copyTimeout = setTimeout(target => {
+                target.textContent = value;
+                copyTimeout	= null;
+            }, 1500, e.currentTarget);
+        });
+
+        const droneYearsSpan = document.getElementById('drone-years');
+        if(droneYearsSpan){
+            droneYearsSpan.textContent = (new Date()).getFullYear() - 2019;
+        }
+
+    }
+
+    const showPopup = name => {
+        const popups = document.getElementById('popups');
+        popups.style.display = 'flex';
+        document.getElementById('popup-' + name).style.display = 'block';
+        setTimeout(() => { popups.classList.add('show'); }, 100);
+    }
+
+})();
